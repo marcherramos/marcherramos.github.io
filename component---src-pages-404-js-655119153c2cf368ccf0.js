669ce514329da8c27c4af9d70fb21e84aabef34e
Arquero, Miguel Angel <miguelangel.arquero.contractor@bbva.com>
2019-04-06 01:11:22 -0500

    publishing blog

---
+++ component---src-pages-404-js-655119153c2cf368ccf0.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{302:function(e,t,n){"use strict";n.r(t);var a=n(0),u=n.n(a);t.default=function(){return u.a.createElement("div",null,u.a.createElement("h1",null,"NOT FOUND"),u.a.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-655119153c2cf368ccf0.js.map
